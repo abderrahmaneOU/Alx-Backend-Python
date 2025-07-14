c0f38a79ede371122eb836b3157b914169f0c6bb
Abderrahmane Oudi <abdoze56@outlook.com>
2025-07-15 00:38:54 +0100

    fix quick error

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -23,7 +23,7 @@ class Message(models.Model):
     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages')
     message_body = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    sent_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Message {self.message_id} from {self.sender}"
