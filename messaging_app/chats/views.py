0e9775fcb2d29a20f6ad095483485760cac5bb10
Abderrahmane Oudi <abdoze56@outlook.com>
2025-07-15 00:47:33 +0100

    fix quick error

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 #! /usr/bin/env python3
 """Viewsets for chats"""
 
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Conversation, Message
@@ -14,6 +14,8 @@ from .serializers import ConversationSerializer, MessageSerializer
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['participants__username']
 
     def perform_create(self, serializer):
         serializer.save()
@@ -22,6 +24,8 @@ class ConversationViewSet(viewsets.ModelViewSet):
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['message_body']
 
     def perform_create(self, serializer):
         serializer.save(sender=self.request.user)
