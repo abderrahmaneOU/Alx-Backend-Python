adbeed800bd8505cc628e237a7d20b044ddb52d5
Abderrahmane Oudi <abdoze56@outlook.com>
2025-07-15 00:31:15 +0100

    Complete messaging_app project: setup, models, serializers, views, URLs, and run migrations

---
+++ messaging_app/chats/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+# Create your views here.
+#! /usr/bin/env python3
+"""Viewsets for chats"""
+
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
