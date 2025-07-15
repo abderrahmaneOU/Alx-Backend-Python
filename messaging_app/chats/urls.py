1be52bee7f9f6b022229991db320e006bce01f56
Abderrahmane Oudi <abdoze56@outlook.com>
2025-07-15 05:29:28 +0100

    add missing keys

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -3,12 +3,16 @@
 
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_nested.routers import NestedDefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
 router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
-router.register(r'messages', MessageViewSet, basename='message')
+
+messages_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
+messages_router.register(r'messages', MessageViewSet, basename='conversation-messages')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(messages_router.urls)),
 ]
