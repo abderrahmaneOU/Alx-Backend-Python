36e6c5dd1ebe87f58df22f056af1fe26c289b912
Abderrahmane Oudi <abdoze56@outlook.com>
2025-07-15 05:25:37 +0100

    add missing keys

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -6,6 +6,8 @@ from rest_framework import viewsets, filters
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class ConversationViewSet(viewsets.ModelViewSet):
@@ -15,10 +17,20 @@ class ConversationViewSet(viewsets.ModelViewSet):
     filter_backends = [filters.SearchFilter]
     search_fields = ['participants__username']
 
+    @action(detail=False, methods=['get'])
+    def status(self, request):
+        count = self.get_queryset().count()
+        return Response({'status': 'ConversationViewSet is active', 'count': count})
+
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated]
     filter_backends = [filters.OrderingFilter]
-    ordering_fields = ['timestamp']
+    ordering_fields = ['sent_at']
+
+    @action(detail=False, methods=['get'])
+    def status(self, request):
+        count = self.get_queryset().count()
+        return Response({'status': 'MessageViewSet is active', 'count': count})
